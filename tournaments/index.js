88495394fb09c6fa5f86136e5b97b275e1556c1d
Gaga <49109890+Gagzgurl@users.noreply.github.com>
2019-04-19 05:11:22 +0200

    Update index.js

--- tournaments/index.js
+++ tournaments/index.js
@@ -286,12 +286,9 @@ class Tournament {
 		if (gameCount > 4) {
 			output.errorReply("Due to high load, you are limited to 4 games at the same time.");
 			return;
-		
+		}
 
 		
-				}
-			}
-		}
 
 		let player = new Rooms.RoomGamePlayer(user, this);
 		let error = this.generator.addUser(player);
